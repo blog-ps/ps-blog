03dc111b91220beceece935d11c7f705c25baa6d
MingCS <347552878@qq.com>
2024-12-05 12:00:47 +0800

    feat: 添加多个新组件，更新样式和动画效果，优化 404 页面，调整颜色变量，新增 SVG 图标

--- src/pages/Home/background.jsx
+++ src/pages/Home/background.jsx
@@ -1,47 +1,85 @@
+import lightgirl from '@/assets/png/light.png';
+import BlurFade from '@/components/ui/blur-fade';
 import Piolet from '@/pages/Home/decorate/piolet';
 import Sphere from '@/pages/Home/decorate/sphere';
-import { useThemeMode } from '@/provider/theme-provider';
 import styled from 'styled-components';
 import Wave from './decorate/wave';
 
-const Background = () => {
-  const { model } = useThemeMode();
+const BackGround = ({ model, children }) => {
   return (
-    <Wrapper>
-      {model === 'dark' && (
+    <>
+      {model === 'dark' ? (
         <>
-          <div className="sphere">
+          <BlurFadeLeft delay={0.25} inView>
             <Sphere />
-          </div>
-          <div className="piolet">
+          </BlurFadeLeft>
+          <BlurFadeRight delay={0.5} inView>
             <Piolet />
-          </div>
+          </BlurFadeRight>
         </>
-      )}
-
-      {model === 'light' && (
+      ) : (
         <div className="wave">
-          <Wave />
+          <BlurFade delay={0.25} inView>
+            <Wave />
+          </BlurFade>
         </div>
       )}
-    </Wrapper>
+      {children}
+      {model === 'light' && (
+        <Imgs className="imgs">
+          <BlurFade inView className="light-girl">
+            <img src={lightgirl} alt="lightgirl" />
+            <div className="smallball" />
+            <div className="bigball" />
+          </BlurFade>
+        </Imgs>
+      )}
+    </>
   );
 };
 
-const Wrapper = styled.div`
-  position: relative;
-  .sphere,
-  .wave {
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-
-  .piolet {
+const Imgs = styled.section`
+  .light-girl {
     position: absolute;
     top: 0;
     right: 0;
+    max-width: 640px;
+  }
+
+  .smallball {
+    z-index: -10;
+    background: #e37e0f29;
+    border-radius: 200px;
+    width: 400px;
+    height: 400px;
+    position: absolute;
+    top: 152px;
+    right: 0;
+  }
+
+  .bigball {
+    z-index: -10;
+    width: 576px;
+    height: 576px;
+    border-radius: 300px;
+    background: #ffffff;
+    opacity: 0.8;
+    position: absolute;
+    top: 255px;
+    right: 180px;
   }
 `;
 
-export default Background;
+const BlurFadeLeft = styled(BlurFade)`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const BlurFadeRight = styled(BlurFade)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+export default BackGround;
